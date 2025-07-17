46b6121f07273e4d344f8b752d28c1905a1efb71
obertjonathan <obertjona@gmail.com>
2025-07-17 12:11:32 +0700

    Init project

---
+++ src/main/java/com/yatta/mondaymirage/MondayMirageApplication.java
@@ -0,0 +1,13 @@
+package com.yatta.mondaymirage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MondayMirageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MondayMirageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yatta/mondaymirage/MondayMirageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yatta.mondaymirage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MondayMirageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
